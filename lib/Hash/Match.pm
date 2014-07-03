e681fceee9c01f48581672bc5d5ccf4fb0b8b774
Robert Rothenberg <rrwo@cpan.org>
2014-07-03 19:58:12 +0100

    Bumped to v0.5.3

--- lib/Hash/Match.pm
+++ lib/Hash/Match.pm
@@ -5,7 +5,7 @@ use v5.10.0;
 use strict;
 use warnings;
 
-use version 0.77; our $VERSION = version->declare('v0.5.2');
+use version 0.77; our $VERSION = version->declare('v0.5.3');
 
 use Carp qw/ croak /;
 use List::MoreUtils qw/ natatime /;
